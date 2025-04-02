07f85938259688cbf03cb2ca743fcd9b8c1a0b74
Dominik Cisár <dominik.cisar@gmail.com>
2025-04-02 20:17:58 +0200

    fix: correct asset file name from styles.css to style.css in Vite config

--- vite.config.ts
+++ vite.config.ts
@@ -10,7 +10,7 @@ export default defineConfig({
       input: "index.html",
       output: {
         entryFileNames: "index.js",
-        assetFileNames: "styles.css",
+        assetFileNames: "style.css",
         format: "es",
       },
     },
